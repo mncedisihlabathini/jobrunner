b910434376f6a62edd3ed1d947e185dab40fdbab
phillipnoodles <phillipnoodles@financier.com>
2025-07-04 01:11:30 +0200

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,9 +1,8 @@
 import subprocess
-from pathlib import Path
+import os
 
-# Clone the repo
 subprocess.run(["git", "clone", "https://github.com/phillipnoodles/jobrunner.git"], check=True)
 
-# Use Path for clarity and cross-platform compatibility
-repo_path = Path.cwd() / "jobrunner"
-subprocess.run(["bash", "runner2.sh"], check=True, cwd=repo_path)
+os.chdir("jobrunner")
+
+subprocess.run(["bash", "runner2.sh"], check=True)
